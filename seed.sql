ea49e32fcd0ae6dde09212a9806b548bdb825331
mbcohen777 <107497234+mbcohen777@users.noreply.github.com>
2022-08-30 15:26:35 -0400

    Add files via upload

--- seed.sql
+++ seed.sql
@@ -1,33 +1,52 @@
-DROP TABLE IF EXISTS property;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS reservation;
+DROP TABLE IF EXISTS properties;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS reservations;
 DROP TABLE IF EXISTS comments;
 
-CREATE TABLE property (
-    prop_id SERIAL PRIMARY KEY,
-    number_street TEXT,
-    US_state TEXT,
-    zip NUMBER
-);
-CREATE TABLE customer (
+CREATE TABLE customers (
     cust_id SERIAL PRIMARY KEY,
-    name_firstlast TEXT,
+    name_firstlast TEXT
 );
 
-CREATE TABLE reservation (
-    CONSTRAINT prop_id FOREIGN KEY(prop_id) REFERENCES property(prop_id), 
-    CONSTRAINT cust_id FOREIGN KEY(cust_id) REFERENCES customer(cust_id),
-    res_date number
+CREATE TABLE properties (
+    prop_id SERIAL PRIMARY KEY,
+    title TEXT,
+    cust_id INT,
+    specs TEXT,
+    about TEXT,
+    prop_space TEXT,
+    guest TEXT,
+    other TEXT,
+    number_street TEXT,
+    US_state TEXT,
+    zip TEXT,
+CONSTRAINT FK_properties_customers FOREIGN KEY(cust_id)
+        REFERENCES customers(cust_id)
+);
+
+
+CREATE TABLE reservations (
+    prop_id INT,
+    cust_id INT,
+    res_date TEXT,
+CONSTRAINT FK_reservations_customers FOREIGN KEY(cust_id)
+        REFERENCES customers(cust_id),
+CONSTRAINT FK_reservations_properties FOREIGN KEY(prop_id)
+        REFERENCES properties(prop_id)
 );
 
 CREATE TABLE comments (
-    CONSTRAINT prop_id FOREIGN KEY(prop_id) REFERENCES property(prop_id), 
-    CONSTRAINT cust_id FOREIGN KEY(cust_id) REFERENCES customer(cust_id),
-    stars NUMBER,
-    comment TEXT
+    prop_id INT, 
+    cust_id INT,
+    stars TEXT,
+    comment TEXT,
+CONSTRAINT FK_comments_customers FOREIGN KEY(cust_id)
+        REFERENCES customers(cust_id),
+CONSTRAINT FK_comments_properties FOREIGN KEY(prop_id)
+        REFERENCES properties(prop_id)
 );
 
-INSERT INTO property (number_street, US_state, zip) VALUES ('1313 mockingbird lane','VA','22306');
-INSERT INTO customer (name_firstlast) VALUES ('Jane Doe');
-INSERT INTO reservation (res_date) VALUES ('20221231');
+INSERT INTO properties (title, specs, about, prop_space, guest, other, number_street, US_state, zip) VALUES ('Sunset Palace', 'lots of specs here', 'Not much about here', '8,000 sqft', 'Enter through the servant cottage', 'no other here','1313 mockingbird lane','VA','22306');
+INSERT INTO customers (name_firstlast) VALUES ('Jane Doe');
+INSERT INTO reservations (res_date) VALUES ('20221231');
 INSERT INTO comments (stars, comment) VALUES ('5','This place is great!');
