120f4d73b9da81648477533b0d19b7e305e06289
mbcohen777@gmail.com <mbcohen777@gmail.com>
2022-08-31 14:06:51 -0400

    1406

--- fec/components/Reservations.js
+++ fec/components/Reservations.js
@@ -10,36 +10,31 @@ const Reservations = ({ property }) => {
         </span>
         <span className="stars">stars</span>
         <br></br>
-        <form className={styles.form}>
-        <input className={styles.check_in}
+        <form onSubmit={console.log("Please wait while you are transferred to our third-party payment site")}className={styles.form}>
+        <input required className={styles.check_in}
           type="text"
           placeholder="check-in YYYMMDD"
         />{" "}
-        <input 
-        className= {styles.check_out}
+        <input required className= {styles.check_out}
         type="text"
         placeholder="check-out YYYMMDD"
       />{" "}
         <br></br>
         <input />
         <br></br>
-        <button className={styles.reserve_button}>Reserve</button>
+        <button type="submit"className={styles.reserve_button}>Reserve</button>
         <p>You won`t be charged yet</p>
         </form>
         <span className={styles.leftrespan}>price X 2 nights</span>
-        <span></span>
         <span className={styles.rightrespan}>price x 2</span>
         <br></br>
         <span className={styles.leftrespan}>Cleaning Fee</span>
-        <span></span>
         <span className={styles.rightrespan}>60</span>
         <br></br>
         <span className={styles.leftrespan}>Service Fee</span>
-        <span></span>
         <span className={styles.rightrespan}>41</span>
         <br></br>
         <span className={styles.leftrespan}>Total Before Taxes</span>
-        <span></span>
         <span className={styles.rightrespan}>price+cleaning+service</span> 
       </>
     );
