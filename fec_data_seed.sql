915b91c8f8838badb24ef5232504158eee786a95
mbcohen777@gmail.com <mbcohen777@gmail.com>
2022-08-31 17:53:05 -0400

    b

--- fec_data_seed.sql
+++ fec_data_seed.sql
@@ -1,17 +1,16 @@
-DROP TABLE IF EXISTS properties;
-DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS customers CASCADE;
 DROP TABLE IF EXISTS reservations;
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE customers (
-    cust_id SERIAL PRIMARY KEY,
+    cust_id SERIAL PRIMARY KEY NOT NULL,
     name_firstlast TEXT
 );
 
 CREATE TABLE properties (
-    prop_id SERIAL PRIMARY KEY,
+    prop_id SERIAL PRIMARY KEY NOT NULL,
     title TEXT,
-    cust_id INT,
     price TEXT,
     specs TEXT,
     about TEXT,
@@ -20,34 +19,27 @@ CREATE TABLE properties (
     other TEXT,
     number_street TEXT,
     US_state TEXT,
-    zip TEXT,
-CONSTRAINT FK_properties_customers FOREIGN KEY(cust_id)
-        REFERENCES customers(cust_id)
+    zip TEXT
 );
 
 
 CREATE TABLE reservations (
     prop_id INT,
-    cust_id INT,
-    res_date TEXT,
-CONSTRAINT FK_reservations_customers FOREIGN KEY(cust_id)
-        REFERENCES customers(cust_id),
-CONSTRAINT FK_reservations_properties FOREIGN KEY(prop_id)
-        REFERENCES properties(prop_id)
+    first_name TEXT,
+    last_name TEXT,
+    guest_num INT,
+    res_date INT
 );
 
 CREATE TABLE comments (
-    prop_id INT, 
-    cust_id INT,
+    first_name TEXT,
+    last_name TEXT,
+    prop_id INT,
     stars TEXT,
-    comment TEXT,
-CONSTRAINT FK_comments_customers FOREIGN KEY(cust_id)
-        REFERENCES customers(cust_id),
-CONSTRAINT FK_comments_properties FOREIGN KEY(prop_id)
-        REFERENCES properties(prop_id)
+    comment TEXT
 );
 
 INSERT INTO properties (title, price, specs, about, prop_space, guest, other, number_street, US_state, zip) VALUES ('Sunset Palace', '150', 'lots of specs here', 'Not much about here', '8,000 sqft', 'Enter through the servant cottage', 'no other here','1313 mockingbird lane','VA','22306');
 INSERT INTO customers (name_firstlast) VALUES ('Jane Doe');
-INSERT INTO reservations (res_date) VALUES ('20221231');
-INSERT INTO comments (stars, comment) VALUES ('5','This place is great!');
+INSERT INTO reservations (prop_id, first_name, last_name, guest_num, res_date) VALUES ('1', 'Bob', 'Testor', '3','20221231');
+INSERT INTO comments (first_name, last_name, prop_id, stars, comment) VALUES ('Erin', 'Smith', '1', '5','This place is great!');
