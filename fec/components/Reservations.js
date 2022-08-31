d730b3d3eb5381e0622795ff789a0ab4d486a8a6
mbcohen777@gmail.com <mbcohen777@gmail.com>
2022-08-31 15:36:15 -0400

    1536

--- fec/components/Reservations.js
+++ fec/components/Reservations.js
@@ -1,43 +1,59 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 
-
 const Reservations = ({ property }) => {
-    return (
-      <div className={styles.reservations_box}> 
-        <span className={styles.price}>
-        price per night 
-        </span>
-        <span className="stars">stars</span>
-        <br></br>
-        <form onSubmit={console.log("Please wait while you are transferred to our third-party payment site")}className={styles.form}>
-        <input required className={styles.check_in}
+  return (
+    <div className={styles.reservations_box}>
+      <span className={styles.price}>price per night</span>
+      <span className="stars">stars</span>
+      <br></br>
+      <form
+        onSubmit={console.log(
+          "Please wait while you are transferred to our third-party payment site"
+        )}
+        className={styles.form}
+      >
+        <input
+          required
+          className={styles.check_in}
           type="text"
           placeholder="check-in YYYMMDD"
         />{" "}
-        <input required className= {styles.check_out}
-        type="text"
-        placeholder="check-out YYYMMDD"
-      />{" "}
+        <input
+          required
+          className={styles.check_out}
+          type="text"
+          placeholder="check-out YYYMMDD"
+        />{" "}
         <br></br>
-        <input />
+        <label> Number of Guests</label>
+        <select>
+          <option value="1"> 1</option>
+          <option value="2"> 2</option>
+          <option value="3"> 3</option>
+          <option value="4"> 4</option>
+          <option value="5"> 5</option>
+          <option value="more than that"> More than that!</option>
+        </select>
         <br></br>
-        <button type="submit"className={styles.reserve_button}>Reserve</button>
+        <button type="submit" className={styles.reserve_button}>
+          Reserve
+        </button>
         <p>You won`t be charged yet</p>
-        </form>
-        <span className={styles.leftrespan}>price X 2 nights</span>
-        <span className={styles.rightrespan}>price x 2</span>
-        <br></br>
-        <span className={styles.leftrespan}>Cleaning Fee</span>
-        <span className={styles.rightrespan}>60</span>
-        <br></br>
-        <span className={styles.leftrespan}>Service Fee</span>
-        <span className={styles.rightrespan}>41</span>
-        <br></br>
-        <span className={styles.leftrespan}>Total Before Taxes</span>
-        <span className={styles.rightrespan}>price+cleaning+service</span> 
-      </div>
-    );
-  };
+      </form>
+      <span className={styles.leftrespan}>price X 2 nights</span>
+      <span className={styles.rightrespan}>price x 2</span>
+      <br></br>
+      <span className={styles.leftrespan}>Cleaning Fee</span>
+      <span className={styles.rightrespan}>60</span>
+      <br></br>
+      <span className={styles.leftrespan}>Service Fee</span>
+      <span className={styles.rightrespan}>41</span>
+      <br></br>
+      <span className={styles.leftrespan}>Total Before Taxes</span>
+      <span className={styles.rightrespan}>price+cleaning+service</span>
+    </div>
+  );
+};
 
 export default Reservations;
