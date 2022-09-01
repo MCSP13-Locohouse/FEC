e7b4291c139c381270e32beba35fbe2075970384
mbcohen777@gmail.com <mbcohen777@gmail.com>
2022-09-01 18:43:28 -0400

    m

--- fec/components/Reservations.js
+++ fec/components/Reservations.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react";;
+import React, { useState } from "react";;
 import styles from "../styles/Home.module.css";
-import axios from "axios";
-
 
 
 const Reservations = () => {
