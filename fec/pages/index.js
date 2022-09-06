36a7e19a47ac9df4d3d89ea1b3fa98643f95d82f
Zach Hudson <hudsonzachpro@gmail.com>
2022-09-06 09:13:16 -0600

    moved axios get

--- fec/pages/index.js
+++ fec/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Description from "../components/Description";
 import Reservations from "../components/Reservations";
-import { Loader } from '@googlemaps/js-api-loader';
+import { Loader } from "@googlemaps/js-api-loader";
 import Map from "../components/Map";
 import React, { Component } from "react";
 import Reviews from "../components/Review";
@@ -34,6 +34,8 @@ export default class App extends Component {
     };
   }
 
+  // componentDidMount() {
+  // }
 
   componentDidMount() {
     axios.get("/api/properties").then((response) => {
@@ -41,10 +43,6 @@ export default class App extends Component {
         property: response.data.properties[0],
       }));
     });
-  }
-
-  componentDidMount() {
-    axios.get("/api/properties").then((res) => {});
 
     axios.get("/api/users").then((res) => {
       this.setState({ users: res.data.users });
@@ -62,20 +60,15 @@ export default class App extends Component {
           <title>Create Next App</title>
         </Head>
 
-        <Description
-          property={this.state.property}
-          handleProperty={this.handleProperty}
-        />
+        <Description property={this.state.property} handleProperty={this.handleProperty} />
 
-        <Reservations property={this.state.property}/>
+        <Reservations property={this.state.property} />
 
         <Reviews reviews={this.state.comments} users={this.state.users} />
 
         <Calendar />
 
-        <Map property={this.props} />
-
-        <Map property={this.props} />
+        {/* <Map property={this.props} /> */}
 
         <main className={styles.main}></main>
 
@@ -85,20 +78,19 @@ export default class App extends Component {
   }
 }
 
+// export async function getServerSideProps() {
+//   // Fetch Lat/Long for given address
+//   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
-export async function getServerSideProps() {
-  // Fetch Lat/Long for given address
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+//   const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=1644+Platte+St,+Denver,+CO+80202&key=${apiKey}`, {
+//     mode: 'cors',
+//     method: 'get'
+//   });
+//   let data = await res.json()
+//   data = data.results[0];
+//   //Lat/long for given address:
+//   let locData = data.geometry.location;
 
-  const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=1644+Platte+St,+Denver,+CO+80202&key=${apiKey}`, {
-    mode: 'cors',
-    method: 'get'
-  });
-  let data = await res.json()
-  data = data.results[0];
-  //Lat/long for given address:
-  let locData = data.geometry.location;
-
-  // Pass data to the page via props
-  return { props: { locData } }
-}
+//   // Pass data to the page via props
+//   return { props: { locData } }
+// }
