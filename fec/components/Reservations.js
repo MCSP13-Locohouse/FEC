5e8bb386234158c01fd9e5ca3efad2c1f3d1257c
Daryle Tan <daryletan@Daryles-MBP.lan>
2022-09-06 16:00:44 -0500

    deploy test

--- fec/components/Reservations.js
+++ fec/components/Reservations.js
@@ -10,19 +10,14 @@ const cleaningFee = 60;
 const serviceFee = 41;
 
 function retNights(date1, date2) {
-  let result = Math.floor((((((date2 - date1)/1000)/60)/60)/24));
+  let result = Math.floor((date2 - date1) / 1000 / 60 / 60 / 24);
   console.log(date2);
   console.log(date1);
-  console.log(result)
+  console.log(result);
   return result;
 }
 
-<<<<<<< HEAD
-const Reservations = ({ property, reservations }) => {
-=======
 const Reservations = ({ property, handleDates }) => {
-  
->>>>>>> ac0588c1f1652cd290023e9f4ed33a2e78238e61
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
@@ -43,7 +38,13 @@ const Reservations = ({ property, handleDates }) => {
     console.log(date[0].startDate);
     console.log(e.currentTarget.value);
   };
-  const resTotal = parseInt(property.price* Math.floor(((((((date[0].endDate) - (date[0].startDate))/1000)/60)/60)/24))) + parseInt(cleaningFee) + parseInt(serviceFee);
+  const resTotal =
+    parseInt(
+      property.price *
+        Math.floor((date[0].endDate - date[0].startDate) / 1000 / 60 / 60 / 24)
+    ) +
+    parseInt(cleaningFee) +
+    parseInt(serviceFee);
 
   return (
     <div className="container">
@@ -90,7 +91,12 @@ const Reservations = ({ property, handleDates }) => {
         <span className={styles.leftrespan}>
           price X {retNights(date[0].startDate, date[0].endDate)} nights
         </span>
-        <span className={styles.rightrespan}>{property.price * Math.floor(((((((date[0].endDate) - (date[0].startDate))/1000)/60)/60)/24))}</span>
+        <span className={styles.rightrespan}>
+          {property.price *
+            Math.floor(
+              (date[0].endDate - date[0].startDate) / 1000 / 60 / 60 / 24
+            )}
+        </span>
         <div className={styles.spacer}></div>
         <br></br>
         <div className={styles.spacer}></div>
