6ef6a437318c1afb0e644a94d92102fea7d3441a
heymj317 <heymj317@users.noreply.github.com>
2022-09-09 19:05:25 -0400

    new icons on map and favicon

--- fec/components/Map.js
+++ fec/components/Map.js
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import { Loader } from "@googlemaps/js-api-loader";
 import axios from "axios";
+import { HomeIcon } from '@heroicons/react/24/solid';
 
 export default function Map(props) {
     //SET UP VARIABLES
@@ -289,14 +290,32 @@ function setMarkers(map, locData) {
     // Origins, anchor positions and coordinates of the marker increase in the X
     // direction to the right and in the Y direction down.
     const image = {
-        url: "https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg",
+        //Pulled the below url from their website:
+        url: 'https://a0.muscache.com/airbnb/static/icons/apple-touch-icon-76x76-3b313d93b1b5823293524b9764352ac9.png',
+        //url: "https://css.gg/home-alt.svg",
+        //url: "https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_B%C3%A9lo.svg",
         // This marker is 20 pixels wide by 32 pixels high.
-        size: new google.maps.Size(100, 210),
+        size: new google.maps.Size(200, 200),
         // The origin for this image is (0, 0).
         origin: new google.maps.Point(0, 0),
         // The anchor for this image is the base of the flagpole at (0, 32).
         anchor: new google.maps.Point(0, 0),
     };
+
+    const svgMarker = {
+        path: "M6 22.8787C4.34315 22.8787 3 21.5355 3 19.8787V9.87866C3 9.84477 3.00169 9.81126 3.00498 9.77823H3C3 9.20227 3.2288 8.64989 3.63607 8.24262L9.87868 2.00002C11.0502 0.828445 12.9497 0.828445 14.1213 2.00002L20.3639 8.24264C20.7712 8.6499 21 9.20227 21 9.77823H20.995C20.9983 9.81126 21 9.84477 21 9.87866V19.8787C21 21.5355 19.6569 22.8787 18 22.8787H6ZM12.7071 3.41423L19 9.70713V19.8787C19 20.4309 18.5523 20.8787 18 20.8787H15V15.8787C15 14.2218 13.6569 12.8787 12 12.8787C10.3431 12.8787 9 14.2218 9 15.8787V20.8787H6C5.44772 20.8787 5 20.4309 5 19.8787V9.7072L11.2929 3.41423C11.6834 3.02371 12.3166 3.02371 12.7071 3.41423Z",
+        fillColor: "white",
+        lineColor: "red",
+        fillOpacity: 80,
+        fillRule: "evenodd",
+        clipRule: "evenodd",
+        strokeWeight: 2,
+        rotation: 0,
+        scale: 2,
+        anchor: new google.maps.Point(15, 30),
+    };
+
+
     // Shapes define the clickable region of the icon. The type defines an HTML
     // <area> element 'poly' which traces out a polygon as a series of X,Y points.
     // The final coordinate closes the poly by connecting to the first coordinate.
