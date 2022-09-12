470434c56fc92795a7b987e5f6f97016bca6c089
heymj317 <heymj317@users.noreply.github.com>
2022-09-12 17:33:00 -0400

    cleaned up code

--- fec/components/Map.js
+++ fec/components/Map.js
@@ -1,8 +1,6 @@
-import { useEffect, useRef } from "react";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { useRef } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
 import axios from "axios";
-import { HomeIcon } from '@heroicons/react/24/solid';
 
 export default function Map(props) {
     //SET UP VARIABLES
@@ -345,36 +343,3 @@ function setMarkers(map, locData) {
         radius: 1400,
     });
 }
-
-
-
-// import { useEffect, useRef } from "react";
-// import { Wrapper, Status } from "@googlemaps/react-wrapper";
-// import { Loader } from "@googlemaps/js-api-loader";
-
-// const Map = (props) => {
-//   const googlemap = useRef(null);
-//   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-//   const lat = props.property.locData.lat;
-//   const long = props.property.locData.lng;
-//   //Will need to get location lat long props
-
-//   useEffect(() => {
-//     const loader = new Loader({
-//       apiKey: apiKey,
-//       version: "weekly",
-//     });
-//     let map;
-//     loader.load().then(() => {
-//       const google = window.google;
-//       map = new google.maps.Map(googlemap.current, {
-//         center: { lat: lat, lng: long },
-//         zoom: 15,
-//       });
-//     });
-//   });
-
-//   return <div id="map" ref={googlemap} />;
-// };
-
-// export default Map;
