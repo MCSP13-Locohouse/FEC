b172063d36e63556dfd636f3bd519591741b9532
Zach Hudson <hudsonzachpro@gmail.com>
2022-09-15 09:37:18 -0600

    Removed unused code

--- fec/pages/index.js
+++ fec/pages/index.js
@@ -9,9 +9,6 @@ import axios from "axios";
 import Header from "../components/Header";
 import Map, { StaticGoogleMap, Marker, Path } from "../components/Map";
 import Gallery from "../components/Gallery";
-// import useSWR from "swr";
-
-// const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 export default class App extends Component {
   constructor(props) {
@@ -48,32 +45,6 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    // axios.get("/api/properties").then((res) => {
-    //   this.setState((prevState) => ({
-    //     property: res.data.properties[0],
-    //   }));
-    // });
-
-    // axios.get("/api/users").then((res) => {
-    //   this.setState({
-    //     users: res.data.users,
-    //     host: res.data.users[0].name_firstlast,
-    //   });
-    // });
-
-    // axios.get("/api/comments").then((res) => {
-    //   this.setState({
-    //     comment: res.data.comments,
-    //     stars: res.data.comments[0].stars,
-    //   });
-    // });
-
-    // axios.get("/api/reservations").then((res) => {
-    //   this.setState({
-    //     startDate: res.data.startDate,
-    //     endDate: res.data.endDate,
-    //   });
-    // });
     axios.get("/api/").then((res) => {
       this.setState({
         comment: res.data.comments,
@@ -86,23 +57,6 @@ export default class App extends Component {
       });
     });
   }
-
-  // const { commentsData, commentsError } = useSWR("api/comments", fetcher);
-  // const { propsData, propsError } = useSWR("api/properties", fetcher);
-  // const { reservsData, reservsError } = useSWR("api/reservations", fetcher);
-  // const { usersData, usersError } = useSWR("api/users", fetcher);
-
-  // if (commentsError) return <p>No comments found</p>;
-  // if (!commentsData) return <p>Loading comments...</p>;
-
-  // if (propsError) return <p>No property data found</p>;
-  // if (!propsData) return <p>Loading props...</p>;
-
-  // if (reservsError) return <p>No reservations data found</p>;
-  // if (reservsData) return <p>Loading reservs...</p>;
-
-  // if (usersError) return <p>No users data found</p>;
-  // if (usersData) return <p>Loading users...</p>;
 
   render() {
     return (
@@ -137,26 +91,6 @@ export default class App extends Component {
           <Description property={this.state.property} host={this.state.host} />
 
           <Reviews reviews={this.state.comment} users={this.state.users} />
-
-          {/* <div>
-            <h2>The comments data is:</h2>
-            <p>{commentsData.comments}</p>
-          </div>
-
-          <div>
-            <h2>The properties data is:</h2>
-            <p>{propsData.properties}</p>
-          </div>
-
-          <div>
-            <h2>The reservations data is:</h2>
-            <p>{reservsData.reservations}</p>
-          </div>
-
-          <div>
-            <h2>The users data is:</h2>
-            <p>{usersData.users}</p>
-          </div> */}
 
           <Map location={this.state.property} />
         </main>
